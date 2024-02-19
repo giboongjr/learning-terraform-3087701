9e53e52a2d9265f58ffeea38537458475a66347b
giboongjr <159428533+giboongjr@users.noreply.github.com>
2024-02-19 18:19:03 +0800

    fix2

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_instance.blog.id
+          target_id = "i-0448c718e9107ecc0"
           port = 80
         }
       }
