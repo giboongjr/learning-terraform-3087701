94074c62ba523d7f90b191d7c98835919ddebd66
giboongjr <159428533+giboongjr@users.noreply.github.com>
2024-02-20 14:11:04 +0800

    create new alb

--- main.tf
+++ main.tf
@@ -46,28 +46,13 @@ resource "aws_instance" "blog" {
 }
 
 module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "9.7.0"
+  source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog-alb"
+  name               = "blog-alb"
   load_balancer_type = "application"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-
-  target_groups = 
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
-    }
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   listeners = {
     ex-http-https-redirect = {
@@ -79,7 +64,30 @@ module "alb" {
         status_code = "HTTP_301"
       }
     }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
 
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
   }
 
   tags = {
